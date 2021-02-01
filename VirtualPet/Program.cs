d8e22db770d25a144842e2dabd00fe7604e8303b
Adam Pryatel <adampryatel@gmail.com>
2021-02-01 15:15:57 -0500

    Created menu draft

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -7,6 +7,32 @@ namespace VirtualPet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! Welcome to Virtual Pets");
+
+            Console.WriteLine("New Pet");
+                Console.WriteLine("What Species is Your Pet?");
+                  Console.WriteLine("Please Enter the Name of Your Pet");
+                // Add Pet Class
+                Console.WriteLine("Check On Pet"); // Outputs data of your pet
+            Console.WriteLine("Feed Pet");       
+            Console.WriteLine("Play with Pet");
+            Console.WriteLine("See the Vet");   // see doctor
+
+
+
+
+            Console.WriteLine("Continue With Your Pet");
+                Console.WriteLine("Check On Pet");
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
