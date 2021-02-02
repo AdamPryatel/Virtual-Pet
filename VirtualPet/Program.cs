3552ef161566a5f92b8a6c63467714185f48ffcb
Adam Pryatel <adampryatel@gmail.com>
2021-02-02 14:48:40 -0500

    First Iteration Complete

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,59 +10,75 @@ namespace VirtualPet
             Console.WriteLine("Hello! Welcome to Virtual Pets");
 
 
-            Console.WriteLine("What Species is Your Pet?");
-            Console.WriteLine("Please Enter the Name of Your Pet");
+            Console.WriteLine("What Species is Your Pet? ");
 
+            pet.SetSpecies(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Name of Your Pet: ");
+
+            pet.SetName(Console.ReadLine());
+
+            bool keepPlaying = true;
+            while(keepPlaying)
+            { 
 
                 
-            Console.WriteLine("1. Check On Pet"); // Outputs data of your pet
-            Console.WriteLine("2. Feed Pet");       
-            Console.WriteLine("3. Play with Pet");
-            Console.WriteLine("4. See the Vet");   // see doctor
-            Console.WriteLine("5. Exit Game");
+                Console.WriteLine("1. Check On Pet"); // Outputs data of your pet
+                Console.WriteLine("2. Feed Pet");       
+                Console.WriteLine("3. Play with Pet");
+                Console.WriteLine("4. See the Vet");   // see doctor
+                Console.WriteLine("5. Exit Game");
 
-            string menuChoice = Console.ReadLine();
+                string menuChoice = Console.ReadLine();
 
-            switch (menuChoice)
-            {
-                case "1":
-                    string petName = pet.GetName();
-                    string petSpecies = pet.GetSpecies();
-                    int petBoredom = pet.GetBoredom();
-                    int petHealth = pet.GetHealth();
-                    int petHunger = pet.GetHunger();
-                    Console.WriteLine(petName);
-                    Console.WriteLine(petSpecies);
-                    Console.WriteLine(petName + "'s boredom level is: " + petBoredom);//if/else over 50 = happy less than = bored
-                    Console.WriteLine(petName + "'s health level is: " + petHealth);
-                    Console.WriteLine(petName + "'s hunger level is: " + petHunger);
 
-                    break;
-                case "2":
-                    break;
-                case "3":
-                    break;
-                case "4":
-                    break;
-                case "5":
-                    break;
-                default:
-                    break;
+
+                int petBoredom = pet.GetBoredom();
+                int petHealth = pet.GetHealth();
+                int petHunger = pet.GetHunger();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                    
+                        Console.WriteLine(pet.Name);
+                        Console.WriteLine(pet.Species);
+                        Console.WriteLine(pet.Name + "'s boredom level is: " + petBoredom);//if/else over 50 = happy less than = bored
+                        Console.WriteLine(pet.Name + "'s health level is: " + petHealth);
+                        Console.WriteLine(pet.Name + "'s hunger level is: " + petHunger);
+
+                        break;
+                    case "2":
+                        pet.Feed();
+                        Console.WriteLine("You Fed " + pet.Name);
+                        break;
+                    case "3":
+                        pet.Play();
+                        Console.WriteLine("You played with " + pet.Name);
+                        break;
+                    case "4":
+                        pet.SeeDoctor();
+                        Console.WriteLine("You took " + pet.Name + " to the vet!");
+                        break;
+                    case "5":
+                        Console.WriteLine("Thank's for Playing!");
+                        keepPlaying = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        break;
+
+                }
+
+                pet.Tick();
+
+                Console.WriteLine("Press Any Key to Continue");
+                Console.ReadKey();
+                Console.Clear();
 
             }
 
-            Console.WriteLine("Continue With Your Pet");
-                Console.WriteLine("Check On Pet");
-
-
-
-
-
-
-
-
-
-
+           
 
         }
     }
