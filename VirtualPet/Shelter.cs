40bb6925b6d5f6aafb40935868f77e07d5d4ebab
Adam Pryatel <adampryatel@gmail.com>
2021-02-05 10:26:12 -0500

    Added SelectPet Method

--- VirtualPet/Shelter.cs
+++ VirtualPet/Shelter.cs
@@ -14,7 +14,22 @@ namespace VirtualPet
             ListofPets.Add(pet);
         }
 
+        public Pet SelectPet()
+        {
+            int index = 1;
 
+            Console.WriteLine("Please select a pet from the list:");
+
+            foreach(Pet pet in ListofPets)
+            {
+                Console.WriteLine(index + ". " + pet.Species + " " + pet.Name);
+                index++;
+            }
+
+            int indexSelected = Convert.ToInt32(Console.ReadLine());
+
+            return ListofPets[indexSelected - 1];
+        }
 
     }
 }
